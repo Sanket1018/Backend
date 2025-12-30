b9941b08bf31e11ca96d6ea7377ef9844eb014a3
Sanket <sanketkhokadesk@gmail.com>
2025-12-29 16:13:02 -0800

    Adding code and changes

--- Microservices/userdemo/src/main/java/com/user/userdemo/UserdemoApplication.java
+++ Microservices/userdemo/src/main/java/com/user/userdemo/UserdemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Enable
+
 public class UserdemoApplication {
 
 	public static void main(String[] args) {
